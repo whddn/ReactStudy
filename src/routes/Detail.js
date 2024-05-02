bb227af32d43cfa51af0add6cf517d7263912e88
이종우 <ghjkl133120@naver.com>
2024-05-02 11:00:00 +0900

    docs: Lifecycle과 useEffect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,26 +1,43 @@
 /*eslint-disable*/
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
-let YellowBtn = styled.button`
-  background: ${props => props.bg };
-  color: ${props => props.bg == 'blue' ? 'white' : 'black' };
-  padding: 10px;
-`
 
 
 
 function Detail(props){
 
+  
   let {id} = useParams();
   let 찾은상품 = props.shoes.find(function(x){
     return x.id == id
   });
+  
+  let [count, setCount] = useState(0)
+  let [alert, setAlert] = useState(true)
+
+
+  useEffect(()=>{
+    let a = setTimeout(()=> { setAlert(false) }, 2000)
+    return ()=>{
+      clearTimeout(a)
+    }
+  }, [])
+  
 
   return(
     <div className="container">
-      <YellowBtn bg="blue">버튼</YellowBtn>
-
+      {
+        alert == true
+        ? <div className="alert alert-warning">
+        2초이내 구매시 할인
+          </div>
+         : null
+      }
+      <button onClick={()=>{
+        setCount(count+1)
+      } }>버튼</button>
       <div className="row">
         <div className="col-md-6">
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
